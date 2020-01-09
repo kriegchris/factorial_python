2fe29c0611d8cca6bb62a3bf66f521a94156bdf3
kriegchris <chriskrieg95@gmail.com>
2020-01-09 11:25:21 -0500

    refactored factorial and main method, wrote new tests

--- factorial.py
+++ factorial.py
@@ -1,10 +1,22 @@
 import sys
 
 def factorial(n):
-    pass
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
 
 def main(args):
-    pass
-
+    print("Welcome to the Factorial Calulator!")
+    num = -1
+    cont = "y"
+    while (cont == "y") :
+        while (num < 1 or num > 10) :
+            num = input("enter an Integer that's greater than 0, but less than 10: ")
+            num = int(num)
+        result = factorial(num)
+        print("the factorial of " + str(num) + " is " + str(result))
+        cont = input("Continue? (y/n): ")
+ 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
--- factorial_test.py
+++ factorial_test.py
@@ -5,6 +5,12 @@ class factorial_test(unittest.TestCase):
     
     def test_factorial(self):
         self.assertEqual(factorial.factorial(1), 1)
+        self.assertEqual(factorial.factorial(2), 2)
+        self.assertEqual(factorial.factorial(3), 6)
+        self.assertEqual(factorial.factorial(4), 24)
+        self.assertEqual(factorial.factorial(5), 120)
+        self.assertEqual(factorial.factorial(6), 720)
+        self.assertEqual(factorial.factorial(7), 5040)
 
 if __name__ == '__main__':
     unittest.main()
